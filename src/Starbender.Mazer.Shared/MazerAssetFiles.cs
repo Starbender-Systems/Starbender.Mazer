3e70bc88eb54a7b6ecbd8bb8f58b976658aeb0f6
Dean Fuqua <alvahdean@gmail.com>
2026-04-04 17:47:01 -0400

    Rename Starbender.Mazer to Starbender.Mazer.Blazor

--- src/Starbender.Mazer.Shared/MazerAssetFiles.cs
+++ src/Starbender.Mazer.Shared/MazerAssetFiles.cs
@@ -3,11 +3,11 @@ namespace Starbender.Mazer;
 public static class MazerAssetFiles
 {
     public const string MudBlazorCss = "_content/MudBlazor/MudBlazor.min.css";
-    public const string ThemeCss = "_content/Starbender.Mazer/mazer.css";
+    public const string ThemeCss = "_content/Starbender.Mazer.Blazor/mazer.css";
     public const string MvcLayoutCss = "_content/Starbender.Mazer.Mvc/layout.css";
     public const string GoogleFontCss = "_content/Starbender.Mazer.Mvc/googlefonts.css";
 
     public const string MudBlazorJs = "_content/MudBlazor/MudBlazor.min.js";
-    public const string ThemeModeJs = "_content/Starbender.Mazer/mazer-mode.js";
+    public const string ThemeModeJs = "_content/Starbender.Mazer.Blazor/mazer-mode.js";
     public const string MvcLayoutJs = "_content/Starbender.Mazer.Mvc/layout.js";
 }
